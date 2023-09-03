08442b70512143d4facd2c4e9633d1db1d5d33bb
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2023-09-03 19:13:01 +0700

    nyoba

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,9 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+           {rel:'StyleSheet',href:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"}],
+    scripts:[{src:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"}]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
